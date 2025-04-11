14c67fa3a90e7ac4392491449b725354996a61c5
tiendung2306 <tiendunglop3c@gmail.com>
2025-04-11 11:39:23 +0700

    initial tailwind primevue

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav>
+      <nav class="flex space-x-4">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <div class="login-container">
+    <div class="counter">
+      <p>Count: {{ count }}</p>
+      <Button @click="increment" label="Submit" />
+    </div>
+
+    <main class="login-content">
+      <h1>Đăng nhập</h1>
+      <form>
+        <!-- Các trường đăng nhập -->
+      </form>
+    </main>
+  </div>
+</template>
